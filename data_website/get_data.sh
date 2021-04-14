e7aa7897b20fe91dc861c5dc9f84df1a7e949466
Carlos Peralta <carlos9917@gmail.com>
2021-04-14 14:01:33 +0000

    Added command line argument to get_data.sh

--- data_website/get_data.sh
+++ data_website/get_data.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 #Get data from website, update database
+#If file name provided it will only convert dats to UTM
+
 today=`date +'%Y%m%d'`
-echo "Downloading data from gimli.dmi.dk"
-curl http://gimli.dmi.dk:8081/glatinfoservice/GlatInfoServlet?command=stationlist | iconv -f iso8859-1 -t utf-8 > station_data_$today.csv
+if [ -z $1 ]; then
+  CSV=station_data_$today.csv
+  echo "Downloading data from gimli.dmi.dk"
+  curl http://gimli.dmi.dk:8081/glatinfoservice/GlatInfoServlet?command=stationlist | iconv -f iso8859-1 -t utf-8 > $CSV
+else
+  CSV=$1
+  echo "File provided by user: $CSV. Doing only lat/lon to UTM conversion"
+fi
 
 #echo Store station data in sqlite
 #Discarding this for the moment
