84bb1130d998685bbecbf86720d905d8df28d49f
Carlos Peralta <carlos9917@gmail.com>
2022-11-21 21:22:32 +0100

    Local commits for container in work laptop

--- container/env.sh
+++ container/env.sh
@@ -5,10 +5,13 @@ export CUSER="roaduser"
 export PID=$(id -u $USER)
 export GID=$(id -g $USER)
 
-export IMAGE="devhub.dmi.dk/cap/shadows_basic"
+#for bare bones python
+#export IMAGE="devhub.dmi.dk/cap/shadows_basic"
+#for grass image
+export IMAGE="devhub.dmi.dk/cap/shadows_grass"
+
 alias docker_clean_images='docker rmi $(docker images -a --filter=dangling=true 
 -q)'
 alias brute_force_clean='docker rmi $(docker images -a)'
-alias docker_clean_ps='docker rm $(docker ps --filter=status=exited --filter=sta
-tus=created -q)'
+alias docker_clean_ps='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
 
--- container/run_docker.sh
+++ container/run_docker.sh
@@ -1,2 +1,7 @@
 source ./env.sh
-docker run -it --rm -p 9696:9696 $IMAGE
+#server
+#docker run -it --rm -p 9696:9696 $IMAGE
+
+#just bash
+docker run -it --name testroad --rm -p 0.0.0.0:9696:9696 $IMAGE
+#docker exec -ti $IMAGE bash
